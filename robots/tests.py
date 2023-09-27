daece350cb8760ad5ad4829e804b6630ab97fc7f
antonovmike <antonovma@outlook.com>
2023-09-27 07:33:14 +0400

    AssertionError

--- robots/tests.py
+++ robots/tests.py
@@ -1,5 +1,3 @@
-import json
-
 from django.test import TestCase
 
 # Create your tests here.
@@ -10,18 +8,25 @@ from .models import Robot
 import json
 
 
+from django.utils.timezone import make_aware
+from datetime import datetime
+
+naive_datetime = datetime.strptime("2022-12-31 23:59:59", "%Y-%m-%d %H:%M:%S")
+aware_datetime = make_aware(naive_datetime)
+
 
 class RobotTestCase(TestCase):
-    def setup(self):
+    def setUp(self):
         self.client = Client()
-        self.robot_data = {"model": "R2", "version": "D2", "created": "2022-12-31 23:59:59"}
+        self.robot_data = {"model": "R2", "version": "D2", "created": aware_datetime.isoformat()}
 
     def test_api_can_create_a_robot(self):
         response = self.client.post('/robots/', data=json.dumps(self.robot_data), content_type='application/json')
         self.assertEqual(response.status_code, 200)
 
     def test_api_can_get_a_robot(self):
-        # response = self.client.post('/robots/', data=json.dumps(self.robot_data), content_type='application/json')
+        response = self.client.post('/robots/', data=json.dumps(self.robot_data), content_type='application/json')
+        self.assertEqual(response.status_code, 200)
         robot = Robot.objects.get()
         response = self.client.get(f'/robots/{robot.id}/')
         self.assertEqual(response.status_code, 200)

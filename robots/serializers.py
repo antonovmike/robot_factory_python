3e857f15a510cf4707b196caf4adfa024c185f2f
antonovmike <antonovma@outlook.com>
2023-09-27 08:03:04 +0400

    new serializers.py, AssertionError

--- robots/serializers.py
+++ robots/serializers.py
@@ -1,7 +1,23 @@
-# from rest_framework import serializers
-# from .models import Robot
+from .models import Robot
+from django.core.serializers import Serializer
+import json
 
-# class RobotSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Robot
-#         fields = ['model', 'version', 'created']
+class RobotSerializer(Serializer):
+    def serialize(self, queryset, **options):
+        # Преобразование queryset в список словарей
+        data = [
+            {'model': item.model, 'version': item.version, 'created': item.created}
+            for item in queryset
+        ]
+        # Преобразование списка словарей в JSON
+        return json.dumps(data)
+
+    def deserialize(self, stream_or_string, **options):
+        # Преобразование JSON в список словарей
+        data = json.loads(stream_or_string)
+        # Создание объектов Robot на основе данных из списка словарей
+        robots = [
+            Robot(model=item['model'], version=item['version'], created=item['created'])
+            for item in data
+        ]
+        return robots

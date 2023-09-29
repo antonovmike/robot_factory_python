64c045b06fa9155e60721b0d474cd87a84e4d797
antonovmike <antonovma@outlook.com>
2023-09-29 09:02:10 +0400

    tests ok

--- robots/tests.py
+++ robots/tests.py
@@ -21,3 +21,27 @@ class RobotTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(Robot.objects.count(), 1)
         self.assertEqual(Robot.objects.get().model, 'R2')
+
+
+class RobotViewTest(TestCase):
+    def test_create_robot(self):
+        url = reverse('create_robot')
+        data = {
+            "model": "R2",
+            "version": "D2",
+            "created": "2022-12-31 23:59:59"
+        }
+        response = self.client.post(url, json.dumps(data), content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(Robot.objects.count(), 1)
+        self.assertEqual(Robot.objects.get().model, 'R2')
+
+        from django.core.files.uploadedfile import TemporaryUploadedFile
+
+    def test_download_report(self):
+        url = reverse('download_report')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response['Content-Disposition'], 'attachment; filename=robots_report.xlsx')
+        self.assertEqual(response['Content-Type'], 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        

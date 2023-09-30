166827ef13e530abe939e0cd94756424189b70e9
antonovmike <antonovma@outlook.com>
2023-09-30 13:45:21 +0400

    new tests

--- robots/tests.py
+++ robots/tests.py
@@ -7,17 +7,24 @@ from .models import Robot
 import json
 
 class RobotTest(TestCase):
-    def setUp(self):
-        self.client = Client()
-
-    def test_create_robot(self):
-        url = reverse('create_robot')
-        data = {
+    @classmethod
+    def setUpTestData(cls):
+        cls.client = Client()
+        cls.url = reverse('create_robot')
+        cls.data = {
             "model": "R2",
             "version": "D2",
             "created": "2022-12-31 23:59:59"
         }
-        response = self.client.post(url, json.dumps(data), content_type='application/json')
+        
+    def create_robot_status_code(self):
+        response = self.client.post(self.url, json.dumps(self.data), content_type='application/json')
         self.assertEqual(response.status_code, 200)
+
+    def create_robot_object_count(self):
+        self.client.post(self.url, json.dumps(self.data), content_type='application/json')
         self.assertEqual(Robot.objects.count(), 1)
+
+    def create_robot_model(self):
+        self.client.post(self.url, json.dumps(self.data), content_type='application/json')
         self.assertEqual(Robot.objects.get().model, 'R2')

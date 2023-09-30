c25be281468442124d4aac9ad0b31e8ab99f4536
antonovmike <antonovma@outlook.com>
2023-09-30 13:52:29 +0400

    random generated names

--- robots/tests.py
+++ robots/tests.py
@@ -3,7 +3,10 @@ from django.test import TestCase
 # Create your tests here.
 from django.test import Client
 from django.urls import reverse
+from django.utils.crypto import get_random_string
+
 from .models import Robot
+
 import json
 
 class RobotTest(TestCase):
@@ -11,14 +14,14 @@ class RobotTest(TestCase):
     def setUpTestData(cls):
         cls.client = Client()
         cls.url = reverse('create_robot')
-        cls.data = {
-            "model": "R2",
-            "version": "D2",
-            "created": "2022-12-31 23:59:59"
-        }
         
     def create_robot_status_code(self):
-        response = self.client.post(self.url, json.dumps(self.data), content_type='application/json')
+        data = {
+            "model": get_random_string(length=2, allowed_chars='ABCDEFGHIJKLMNOT123456789'),
+            "version": get_random_string(length=2, allowed_chars='ABCDEFGHIJKLMNOT123456789'),
+            "created": "2022-12-31 23:59:59"
+        }
+        response = self.client.post(self.url, json.dumps(data), content_type='application/json')
         self.assertEqual(response.status_code, 200)
 
     def create_robot_object_count(self):

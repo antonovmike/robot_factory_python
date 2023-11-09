9c4e2a802386bf34ba8d4a252755f6331ce162f4
antonovmike <antonovma@outlook.com>
2023-11-09 10:00:39 +0400

    add robot_serial to Order

--- orders/models.py
+++ orders/models.py
@@ -3,6 +3,7 @@ from robots.models import Robot
 
 
 class Order(models.Model):
+    robot_serial = models.CharField(max_length=5)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='orders_orders')
     robot = models.ForeignKey(Robot, on_delete=models.CASCADE)
     order_date = models.DateTimeField(blank=False, null=False)

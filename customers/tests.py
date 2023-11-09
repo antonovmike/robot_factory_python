228311ba26f9a92f610ac8a8ec5904d611488e68
antonovmike <antonovma@outlook.com>
2023-11-09 15:49:55 +0400

    CustomerTestCase

--- customers/tests.py
+++ customers/tests.py
@@ -2,27 +2,28 @@ from django.test import TestCase
 from .models import Customer
 from .serializers import CustomerSerializer
 
+# "sırsöz" / "сырсөз" means "password" in Kyrgyz language
 class CustomerTestCase(TestCase):
     def setUp(self):
         self.customer = Customer.objects.create(
-            name='John Doe',
-            email='john@example.com',
-            login='johndoe',
-            password='secret'
+            name='Kurmanjan Datka',
+            email='kurmanjan@example.com',
+            login='kurmanjandatka',
+            password='syrsoz'
         )
 
     def test_customer_creation(self):
-        self.assertEqual(self.customer.name, 'John Doe')
-        self.assertEqual(self.customer.email, 'john@example.com')
-        self.assertEqual(self.customer.login, 'johndoe')
-        self.assertEqual(self.customer.password, 'secret')
+        self.assertEqual(self.customer.name, 'Kurmanjan Datka')
+        self.assertEqual(self.customer.email, 'kurmanjan@example.com')
+        self.assertEqual(self.customer.login, 'kurmanjandatka')
+        self.assertEqual(self.customer.password, 'syrsoz')
 
     def test_customer_serializer(self):
         serializer = CustomerSerializer(instance=self.customer)
         expected_data = {
-            'name': 'John Doe',
-            'email': 'john@example.com',
-            'login': 'johndoe',
-            'password': 'secret'
+            'name': 'Kurmanjan Datka',
+            'email': 'kurmanjan@example.com',
+            'login': 'kurmanjandatka',
+            'password': 'syrsoz'
         }
         self.assertEqual(serializer.data, expected_data)

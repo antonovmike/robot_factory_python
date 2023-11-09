1d98362d3909575bb3b7877bf2523d7a038fc9f1
antonovmike <antonovma@outlook.com>
2023-11-09 15:44:12 +0400

    CustomerTestCase

--- customers/tests.py
+++ customers/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from .models import Customer
+from .serializers import CustomerSerializer
 
-# Create your tests here.
+class CustomerTestCase(TestCase):
+    def setUp(self):
+        self.customer = Customer.objects.create(
+            name='John Doe',
+            email='john@example.com',
+            login='johndoe',
+            password='secret'
+        )
+
+    def test_customer_creation(self):
+        self.assertEqual(self.customer.name, 'John Doe')
+        self.assertEqual(self.customer.email, 'john@example.com')
+        self.assertEqual(self.customer.login, 'johndoe')
+        self.assertEqual(self.customer.password, 'secret')
+
+    def test_customer_serializer(self):
+        serializer = CustomerSerializer(instance=self.customer)
+        expected_data = {
+            'name': 'John Doe',
+            'email': 'john@example.com',
+            'login': 'johndoe',
+            'password': 'secret'
+        }
+        self.assertEqual(serializer.data, expected_data)
